488d89abe205e1a700660386095b1d65258dd4d8
Frederik Vilhelmsen <frederik@rovi.dk>
2023-01-12 12:54:46 +0100

    docker setup

---
+++ mariadb/db.sql
@@ -0,0 +1,26 @@
+create database if not exists scuffedware;
+
+use scuffedware;
+
+create table if not exists devices
+(
+    `id`            int unsigned auto_increment,
+    `name`          varchar(255) not null,
+    `created_at`    timestamp not null default CURRENT_TIMESTAMP(),
+    `updated_at`    timestamp not null default CURRENT_TIMESTAMP() on update CURRENT_TIMESTAMP(),
+    constraint `devices_pk`
+        primary key (`id`)
+);
+
+create table if not exists device_logs
+(
+    `id`            int unsigned auto_increment,
+    `device_id`     int unsigned not null,
+    `message`       varchar(255) not null,
+    `created_at`    timestamp not null default CURRENT_TIMESTAMP(),
+    `updated_at`    timestamp not null default CURRENT_TIMESTAMP() on update CURRENT_TIMESTAMP(),
+    constraint `device_logs_pk`
+        primary key (`id`),
+    constraint `device_logs_devices_fk`
+        foreign key (`device_id`) references devices (`id`)
+);
