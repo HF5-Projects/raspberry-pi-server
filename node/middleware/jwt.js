cde8787641aa8d89314ed471dde3abc72ed3c74e
Frederik Vilhelmsen <frederik@rovi.dk>
2023-01-16 12:54:58 +0100

    small fix :)

--- node/middleware/jwt.js
+++ node/middleware/jwt.js
@@ -5,7 +5,7 @@ function authenticateToken (req, res, next){
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, bufferprocess.env.TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
         next();
