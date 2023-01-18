2a0ce869050c1c425c8e8c76b5fc6278cb104958
Victor <victorneerholt@gmail.com>
2023-01-18 08:40:56 +0100

    Made get,post,put,delete

--- node/routes/testApi.js
+++ node/routes/testApi.js
@@ -5,17 +5,65 @@ var dbconn = require('../database/dbconn');
 
 router.use(auth);
 
-router.post('/testApi', async function(req, res, next) {
+router.get('/testApi', async (req, res) => {
     try {
+        const name = req.body.name;
         const conn = await dbconn.getConnection();
-        const rows = await conn.query("SELECT * FROM devices");
+        const rows = await conn.query(`SELECT * FROM devices`);
         conn.release();
-        res.json(rows);
+        res.status(200).send(rows);
     }
     catch (err) {
         console.error(err);
         res.sendStatus(500);
     }
-});
+  })
+
+
+
+  router.post('/testApi', async (req, res) => {
+    try {
+        const name = req.body.name;
+        const conn = await dbconn.getConnection();
+        const rows = await conn.query(`INSERT INTO devices (name) VALUES ('${name}')`);
+        conn.release();
+        res.sendStatus(201);
+    }
+    catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+  })
+
+
+  router.put('/testApi/:name/id/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const name = req.params.name;
+        const conn = await dbconn.getConnection();
+        const rows = await conn.query(`UPDATE devices SET name = ('${name}') WHERE id = ('${id}')`);
+        conn.release();
+        res.sendStatus(201);
+    }
+    catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+  })
+
+
+  router.delete('/testApi/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const conn = await dbconn.getConnection();
+        const rows = await conn.query(`DELETE FROM devices WHERE id = ('${id}')`);
+        conn.release();
+        res.sendStatus(201);
+    }
+    catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+  })
 
 module.exports = router;
