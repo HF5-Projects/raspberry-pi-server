1c353dbf00ebd3d35f8e5bad1bc107c3e8db8c92
Victor <victorneerholt@gmail.com>
2023-01-23 08:25:18 +0100

    first frontend

---
+++ frontend/components/appHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h1>Scuffedware</h1>
+        <ul>
+            <li>
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/settings">Settings</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/data">Data</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default{
+    name: "appHeader"
+}
+</script>
+
+<style>
+
+</style>
