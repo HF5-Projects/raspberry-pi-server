5272295d33301741be634012886a9755a8c4065e
Victor <victorneerholt@gmail.com>
2023-01-23 10:50:53 +0100

    added axios call

--- frontend/pages/devices.vue
+++ frontend/pages/devices.vue
@@ -3,8 +3,31 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import AppHeader from '~/components/appHeader.vue';
 
-export default Vue.extend({ components: { AppHeader } })
+import  axios  from "axios";
+
+export default{
+    data(){
+        return{
+            devicesData: []
+        }
+    },
+    async created(){
+        const config = {
+            headers:{
+                'Accept': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTZXJ2ZXJBUEkiLCJuYW1lIjoiU2VydmVyQVBJVG9rZW4iLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTk2OTAwMjU2MiwiaXNzIjoiU2VydmVyQVBJIiwiYXVkIjoiU2VydmVyQVBJVXNlcnMifQ.yJ7gnG7GXkJvL1oECQ7bdIs5JVC-X1gUUe87q7Wm42U'
+            }
+        }
+
+        try {
+            const res = await axios.get("http://192.168.1.10/api/arduinoDevice/", config);
+            this.devicesData = res.data;
+            console.log(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }   
+}
+
 </script>
